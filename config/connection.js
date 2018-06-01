f77db5a6131e1eee4d2d49334c27e632f320bbc9
Dan Fabry <dfabry14@gmail.com>
2018-05-31 20:19:46 -0500

    site files

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,16 @@
 var mysql = require("mysql");
 
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'burger_db'
+  })
+}
+
 var connection = mysql.createConnection({
   host: "localhost",
   port: 8889,
